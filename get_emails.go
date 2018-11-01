63666d31abcf8fd84edeb249b32fbeeb22ab873c
randiaz95 <randiaz95@gmail.com>
2018-10-31 22:00:00 -0400

    Add files via upload
    Extract emails in a file.

---
+++ get_emails.go
@@ -0,0 +1,41 @@
+package main
+
+import ("fmt"; "os"; "bufio"; "regexp"; "strings")
+
+func main() {
+    
+    var filename string = "entire_state_v.lst"
+    var output []string = extract_email(filename)
+    fmt.Println(output)
+
+}
+
+
+func extract_email(filename string) []string {
+    // Get filename and get all emails
+
+    var output []string = []string{}
+    file, err := os.Open(filename)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    reg, err := regexp.Compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+    if err != nil {
+        fmt.Println("ERROR: ", err)
+        panic(err)
+    }
+
+    file_scanner := bufio.NewScanner(file)
+    var word string
+    for file_scanner.Scan() {
+        for _, word = range strings.Fields(file_scanner.Text()) {
+            if reg.MatchString(word) {
+                output = append(output, word)
+            }
+        }
+    }
+
+    return output
+
+}
