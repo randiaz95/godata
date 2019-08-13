9925294602541fec296f10ff33f35694339aa53e
randiaz95 <randiaz95@gmail.com>
2019-08-13 09:32:27 -0400

    Add files via upload

---
+++ extract_links.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	foundUrls := make(map[string]bool)
+	seedUrls := os.Args[1:]
+
+	// Channels
+	chUrls := make(chan string)
+	chFinished := make(chan bool)
+
+	// Kick off the crawl process (concurrently)
+	for _, url := range seedUrls {
+		go crawl(url, chUrls, chFinished)
+	}
+
+	// Subscribe to both channels
+	for c := 0; c < len(seedUrls); {
+		select {
+		case url := <-chUrls:
+			foundUrls[url] = true
+		case <-chFinished:
+			c++
+		}
+	}
+
+	// We're done! Print the results...
+
+	fmt.Println("\nFound", len(foundUrls), "unique urls:\n")
+	file, err := os.Create("miami_links/" + time.Now().String() + ".txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	for url, _ := range foundUrls {
+		_, err := file.WriteString(url + "\n")
+		if err != nil {
+			fmt.Println("Failed to write ", url)
+		}
+	}
+
+	close(chUrls)
+	close(chFinished)
+}
+
+// Helper function to pull the href attribute from a Token
+func getHref(t html.Token) (ok bool, href string) {
+	// Iterate over all of the Token's attributes until we find an "href"
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	// "bare" return will return the variables (ok, href) as defined in
+	// the function definition
+	return
+}
+
+// Extract all http** links from a given webpage
+func crawl(url string, ch chan string, chFinished chan bool) {
+	resp, err := http.Get(url)
+
+	defer func() {
+		// Notify that we're done after this function
+		chFinished <- true
+	}()
+
+	if err != nil {
+		fmt.Println("ERROR: Failed to crawl \"" + url + "\"")
+		return
+	}
+
+	b := resp.Body
+	defer b.Close() // close Body when the function returns
+
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			// Check if the token is an <a> tag
+			isAnchor := t.Data == "a"
+			if !isAnchor {
+				continue
+			}
+
+			// Extract the href value, if there is one
+			ok, url := getHref(t)
+			if !ok {
+				continue
+			}
+
+			// Make sure the url begines in http**
+			hasProto := strings.Index(url, "http") == 0
+			if hasProto {
+				ch <- url
+			}
+		}
+	}
+}
